cf75e1963e52b471ecda516990805275dc691340
Ioannidis <panos277@hotmail.com>
2021-04-10 18:08:22 +0300

    Used argparser

--- createData.py
+++ createData.py
@@ -1,31 +1,40 @@
-import collections
-import getopt
-import json
+import argparse
 import sys
 
 from dataGenerator import DataGenerator
 
+args_parser = argparse.ArgumentParser(description='kvServer')
+args_parser.add_argument('-k', '--keyFile', dest='k', type=str, default='keyFile.txt', metavar='', required=True,
+                         help='Contains the key, type pairs.')
+args_parser.add_argument('-n', '--numberOfLines', dest='n', type=int, metavar='', required=True,
+                         help='Indicates the number of lines.')
+args_parser.add_argument('-d', '--nesting', dest='d', type=int, metavar='', required=True,
+                         help='The maximum level of nesting.')
+args_parser.add_argument('-m', '--numberOfKeys', dest='m', type=int, metavar='', required=True,
+                         help='The maximum number of keys inside each value.')
+args_parser.add_argument('-l', '--length', dest='l', type=int, metavar='', required=True,
+                         help='The maximum length of a string value whenever you need to generate a string.')
+args = args_parser.parse_args()
 
-available_options = "k:n:d:l:m:"
-options = collections.defaultdict()
+if args.n < 1:
+    sys.exit('[Error] Number of lines should be greater than 0.')
+elif args.d < 0:
+    sys.exit('[Error] Nesting should be greater equal to 0.')
+elif args.m < 0:
+    sys.exit('[Error] The maximum number of keys inside each value should be greater equal to 0.')
+elif args.l < 1:
+    sys.exit('[Error] The maximum length of a string should be greater than 0.')
+
 try:
-    args, values = getopt.getopt(sys.argv[1:], available_options)
+    f = open(args.k, 'r')
+    lines = f.readlines()
+    f.close()
+except FileNotFoundError as e:
+    sys.exit(f'{e}')
 
-    for k, v in args:
-        options[k[1:]] = v
-
-except getopt.error as err:
-    print(str(err))
-    sys.exit(2)
-
-print(options.items())
-
-f = open(options['k'], 'r')
-lines = f.readlines()
-f.close()
 keys = [(line.strip().split(' ')) for line in lines]
 
-data_generator = DataGenerator(options, keys)
+data_generator = DataGenerator(args, keys)
 data_generator.generate()
 data = data_generator.get_data()
 
